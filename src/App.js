872717fc8d44bcce99300da764e3609ca5777183
Zack Woo <zwoo@fandango.com>
2022-08-28 11:58:02 -0700

    basic blocks

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      <header className="app__header">
+        <h1>Slide Solutions</h1>
       </header>
+      <div className="app__wrap">
+        <ul className="app__playlist">
+          <li className="app__track">Track 1</li>
+          <li className="app__track">Track 2</li>
+          <li className="app__track">Track 3</li>
+          <li className="app__track">Track 4</li>
+          <li className="app__track">Track 5</li>
+          <li className="app__track">Track 6</li>
+          <li className="app__track">Track 7</li>
+          <li className="app__track">Track 8</li>
+          <li className="app__track">Track 9</li>
+          <li className="app__track">Track 9</li>
+        </ul>
+        <div className="app__info"></div>
+      </div>
     </div>
   );
 }
