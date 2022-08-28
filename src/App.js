33f920913317a3e36b61b7f8f1b4450cdca8c208
Zack Woo <zwoo@fandango.com>
2022-08-28 12:42:33 -0700

    add drag/drop functionality for the playlist section

--- src/App.js
+++ src/App.js
@@ -2,18 +2,22 @@
 import './App.css';
 import Playlist from './Playlist.js';
 import TrackInfo from './TrackInfo.js';
+import { DndProvider } from 'react-dnd'
+import { HTML5Backend } from 'react-dnd-html5-backend'
 
 function App() {
   return (
-    <div className="app">
-      <header className="app__header">
-        <h1>Slide Solutions</h1>
-      </header>
-      <div className="app__wrap">
-        <Playlist />
-        <TrackInfo />
+    <DndProvider backend={HTML5Backend}>
+      <div className="app">
+        <header className="app__header">
+          <h1>Slide Solutions</h1>
+        </header>
+        <div className="app__wrap">
+          <Playlist />
+          <TrackInfo />
+        </div>
       </div>
-    </div>
+    </DndProvider>
   );
 }
 
--- src/Playlist.js
+++ src/Playlist.js
@@ -1,14 +1,42 @@
-const playlist = () => <ul className="app__playlist">
-  <li className="app__track">Track 1</li>
-  <li className="app__track">Track 2</li>
-  <li className="app__track">Track 3</li>
-  <li className="app__track">Track 4</li>
-  <li className="app__track">Track 5</li>
-  <li className="app__track">Track 6</li>
-  <li className="app__track">Track 7</li>
-  <li className="app__track">Track 8</li>
-  <li className="app__track">Track 9</li>
-  <li className="app__track">Track 9</li>
-</ul>
+import React, { useState, useCallback } from 'react'
+import Track from './Track'
 
-export default playlist;
+const TRACKS = [
+  { id: 1, name: 'dog' },
+  { id: 2, name: 'cat' },
+  { id: 3, name: 'fish' },
+  { id: 4, name: 'hamster' },
+]
+
+const Playlist = () => {
+  const [tracks, setTracks] = useState(TRACKS)
+
+  const moveTrackListItem = useCallback(
+    (dragIndex, hoverIndex) => {
+        const dragItem = tracks[dragIndex]
+        const hoverItem = tracks[hoverIndex]
+        // Swap places of dragItem and hoverItem in the tracks array
+        setTracks(tracks => {
+            const updatedTracks = [...tracks]
+            updatedTracks[dragIndex] = hoverItem
+            updatedTracks[hoverIndex] = dragItem
+            return updatedTracks
+        })
+    },
+    [tracks],
+)
+
+  return (
+      <ul className="app__playlist">{tracks.map((track, index) => (
+        <Track
+            key={track.id}
+            index={index}
+            text={track.name}
+            moveListItem={moveTrackListItem}
+        />
+    ))}
+    </ul>
+  )
+}
+
+export default Playlist;
