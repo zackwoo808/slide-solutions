b3d1ec01e4708a0f3d2586bdc8fe82f6a95bc9fa
Zack Woo <zwoo@fandango.com>
2022-10-29 13:31:09 -0700

    convert id to INT

--- server/lib/stream.js
+++ server/lib/stream.js
@@ -45,7 +45,7 @@ async function getAllPlaylistTracks(playlistId) {
         t.music_key
       from public.tracks as t
       join public.playlist_to_track as p2t
-      on p2t.track_id = t.id and p2t.playlist_id = ${playlistId}
+      on p2t.track_id = t.id and p2t.playlist_id = ${parseInt(playlistId)}
     `;
 
     return playlistTracks;
